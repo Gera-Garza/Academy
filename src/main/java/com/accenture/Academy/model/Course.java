f816ffd3bcd3065056ba1475537df4358f935729
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-08-06 16:07:25 -0500

    creation of model course

---
+++ src/main/java/com/accenture/Academy/model/Course.java
@@ -0,0 +1,57 @@
+package com.accenture.academy.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="course")
+public class Course {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String description;
+    private String lenguage;
+    @Column(name="estimated_date")
+    private Date estimatedDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLenguage() {
+        return lenguage;
+    }
+
+    public void setLenguage(String lenguage) {
+        this.lenguage = lenguage;
+    }
+
+    public Date getEstimatedDate() {
+        return estimatedDate;
+    }
+
+    public void setEstimatedDate(Date estimatedDate) {
+        this.estimatedDate = estimatedDate;
+    }
+}
